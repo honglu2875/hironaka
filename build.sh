6d09ae20c8acb36d7756d43fdac3848ba0310902
Void <honglu2875@gmail.com>
2022-06-18 19:45:39 -0400

    initial commit of cpp branch (working on cpp libraries)

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PROJECT_NAME = "hironaka"
+BUILD_PATH = $PROJECT_NAME"/build"
+CLIB_PATH = $PROJECT_NAME"/cpp"
+
+mkdir $BUILD_PATH
+g++ -fPIC -shared -o $BUILD_PATH/cppUtil.so $CLIB_PATH/cppUtil.cpp
+
