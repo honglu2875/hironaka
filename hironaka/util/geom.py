74dffd06ee292d92a70ff675b612803fac5b32bb
au596283 <gergely.berczi@gmail.com>
2022-06-23 22:07:25 +0200

    update requirements.txt

--- hironaka/util/geom.py
+++ hironaka/util/geom.py
@@ -1,4 +1,10 @@
 from typing import List, Tuple
+import sympy as sp
+from sympy import symbols, IndexedBase, Idx, MatrixSymbol, Matrix, Poly, Sum
+import numpy as np
+from itertools import chain, combinations
+
+
 
 
 def getNewtonPolytope_approx(points: List[Tuple[int]]):
@@ -46,3 +52,51 @@ def shift(points: List[Tuple[int]], coords: List[int], axis: int):
         sum([x[k] for k in coords]) if i == axis else x[i]
         for i in range(DIM)])
         for x in points]
+
+
+def QuadraticPart(Ord: int):
+    """Get the quadratic form q(v)=v_1v_{k-1}+v_2v_{k-2}+...+v_{k-1}v_1 for v_i in C^k in a matrix form: the (i,j) entry of
+       the upper triangular kxk matrix QuadraticPart(k) is the coefficient of e-ie_j in q(v).
+    """
+
+    i = Idx('i', Ord)
+    j = Idx('j', Ord)
+    b = IndexedBase('b')
+    s = symbols('s', integer=True)
+    V = sp.zeros(Ord,Ord)
+    D = []
+    B = MatrixSymbol('b',Ord,Ord)
+    Blower = Matrix(np.tril(Matrix(B).transpose()))
+    for s in range(1,Ord):
+        V = V + Blower.row(s-1).transpose()*Blower.row(Ord-s-1)
+    return Matrix(np.triu(V+V.transpose()-Matrix(np.diag(np.array(V.diagonal())[0]))))
+
+print(QuadraticPart(4))
+print(sp.flatten([QuadraticPart(4)[i,i:] for i in range(QuadraticPart(4).rows)]))
+
+def QuadraticFixedPoints(Ord: int):
+    """
+        Get the torus fixed points...
+    """
+    Q = []
+    for s in range(2,Ord+1):
+        for LinSet in combinations(list(range(Ord)),s):
+            print(combinations(list(LinSet),2))
+            Q.append([list(LinSet), (L for L in combinations(list(LinSet), 2) if L[0] + L[1] < Ord)])
+    return Q
+
+print(QuadraticFixedPoints(3))
+
+
+def ThomMonomialIdeal(Ord: int):
+    """
+        Get the monomial ideal of the rational map in the Thom polynomial paper
+        E.g for k=3 this is a homogeneous degree 2 monomial ideal in C[t,b_22,b_23,b_33]:
+        I_3=(b_22*b_33,t*b_33,t*b_23,b_22^2,t^2)
+    """
+
+
+
+
+#P = Poly(np.array(Blower.row(2).transpose()*Blower.row(1))[0][0]+b[1,2])
+#print(P,P.monoms())
