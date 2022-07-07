9541178d5b5dbc62b9e251d932e28362ddf821ab
Void <honglu2875@gmail.com>
2022-07-07 11:51:26 -0400

    Torch points

--- hironaka/abs/PointsTorch.py
+++ hironaka/abs/PointsTorch.py
@@ -1,53 +1,63 @@
 from typing import List, Any, Dict, Optional, Union
 
 import numpy as np
+import torch
 
 from hironaka.abs.PointsBase import PointsBase
-from hironaka.src import shift_lst, get_newton_polytope_lst, get_shape, scale_points
+from hironaka.src import shift_lst, get_newton_polytope_lst, get_shape, scale_points, get_batched_padded_array
 
 
-class Points(PointsBase):
-    """
-        When dealing with small batches, small dimension and small point numbers, list is much better than numpy.
-    """
-    config_keys = ['value_threshold']
+class PointsTorch(PointsBase):
+    config_keys = ['value_threshold', 'device_key', 'padded_value']
 
     def __init__(self,
-                 points: Union[List[List[List[int]]], np.ndarray],
+                 points: Union[torch.Tensor, List[List[List[int]]], np.ndarray],
                  value_threshold: Optional[int] = 1e8,
+                 device_key: Optional[str] = 'cpu',
+                 padded_value: Optional[float] = -1.0,
                  config_kwargs: Optional[Dict[str, Any]] = None,
                  **kwargs):
         config = kwargs if config_kwargs is None else {**config_kwargs, **kwargs}
         self.value_threshold = value_threshold
 
-        # Be lenient and allow numpy array as input.
-        # The input might already be -1 padded arrays. Thus, we do a thorough check to clean that up.
-        if isinstance(points, np.ndarray):
-            points = points.tolist()
-            assert isinstance(points, list)
-            for p in points:
-                while p[-1][0] == -1:
-                    p.pop()
-                    assert len(p) != 0  # Should not be empty batch
+        # It's better to require a fixed shape of the tensor implementation.
+        if 'max_num_points' not in config:
+            raise Exception("Must have `max_num_points` in parameters.")
+
+        if isinstance(points, list):
+            points = torch.FloatTensor(
+                    get_batched_padded_array(points,
+                                             new_length=config['max_num_points'],
+                                             constant_value=config.get('padded_value', -1)))
+        elif isinstance(points, (torch.Tensor, np.ndarray)):
+            points = torch.FloatTensor(points)
+        else:
+            raise Exception(f"Input must be a Tensor, a numpy array or a nested list. Got {type(points)}.")
+
+        self.device_key = device_key
+        self.padded_value = padded_value
 
         super().__init__(points, **config)
+        self.device = torch.device(self.device_key)
+        self.points.to(self.device)
 
+    # TODO:
     def exceed_threshold(self) -> bool:
         """
             Check whether the maximal value exceeds the threshold.
         """
         if self.value_threshold is not None:
-            for b in range(self.batch_size):
-                for i in range(len(self.points[b])):
-                    if max(self.points[b][i]) > self.value_threshold:
-                        return True
-            return False
+            if torch.max(self.points) >= self.value_threshold:
+                return True
         return False
 
     def get_num_points(self) -> List[int]:
         """
             The number of points for each batch.
         """
+        first_slice = self.points[:, :, 0].ge(0)
+        # TODO:
+
         return [len(batch) for batch in self.points]
 
     def _shift(self,
@@ -78,28 +88,6 @@ class Points(PointsBase):
             assert len(p) != 0
             ended_each_batch.append(True if len(p) <= 1 else False)
         return ended_each_batch
-
-    def get_sym_features(self):
-        """
-            [currently not in-use]
-            Say the points are ((x_1)_1, ...,(x_1)_n), ...,((x_k)_1, ...,(x_k)_n)
-            We generate the Newton polynomials of each coordinate and output the new array as features.
-            The output becomes
-                ((sum_i (x_i)_1^1), ..., (sum_i (x_i)_n^1)),
-                ...,
-                ((sum_i (x_i)_1^length), ..., (sum_i (x_i)_n^length))
-        """
-        features = [
-            [
-                [
-                    sum([
-                        x[i] ** j for x in batch
-                    ]) for i in range(self.dimension)
-                ] for j in range(1, self.max_num_points + 1)
-            ] for batch in self.points
-        ]
-
-        return features
 
     def __repr__(self):
         return str(self.points)
--- test/testPoints.py
+++ test/testPoints.py
@@ -121,3 +121,9 @@ class TestPoints(unittest.TestCase):
         assert str(points) == str(r)
         # p2.shift([[1, 3]], [3])
         # print(p2)
+
+    def test_value_threshold(self):
+        points = Points([[[5e7, 5e7+1, 1e7], [1, 1, 2]]], value_threshold=int(1e8))
+        assert not points.exceed_threshold()
+        points.shift([[0, 1]], [0])
+        assert points.exceed_threshold()
