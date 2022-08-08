9fda7c38d87f197b9b5a749c56aceacaa4465ad2
Void <honglu2875@gmail.com>
2022-08-08 17:53:45 -0400

    Left an optional argument to initialize Trainer and FusedGame with `reward_func`. Added a method `Trainer.replace_reward_func` to override the reward function. Shift the default agent reward by -1 for convergence issue. Minor changes in the validation behaviors of Trainer.

--- hironaka/trainer/player_modules/PlayerNN.py
+++ hironaka/trainer/player_modules/PlayerNN.py
@@ -20,9 +20,39 @@ class ChooseFirstAgentModule(nn.Module, DummyModule):
         self.device = device
 
     def forward(self, x):
-        r = torch.zeros(x['coords'].shape, device=self.device, dtype=torch.float32)
-        r.scatter_(1, x['coords'].argmax(1).unsqueeze(1), 1.)
-        return r
+        return torch.nn.functional.one_hot(x['coords'].argmax(1), num_classes=self.dimension).type(torch.float32)
+
+
+class RandomAgentModule(nn.Module, DummyModule):
+    def __init__(self, dimension: int, max_num_points: int, device: torch.device):
+        super().__init__()
+        self.dimension = dimension
+        self.max_num_points = max_num_points
+        self.device = device
+
+    def forward(self, x):
+        coords = x['coords']
+        r = torch.rand((coords.shape[0], self.dimension), device=self.device) * coords
+        return torch.nn.functional.one_hot(r.argmax(1), num_classes=self.dimension).type(torch.float32)
+
+
+class RandomHostModule(nn.Module, DummyModule):
+    def __init__(self, dimension: int, max_num_points: int, device: torch.device):
+        super().__init__()
+        self.dimension = dimension
+        self.max_num_points = max_num_points
+        self.device = device
+
+        self.action_map = torch.zeros((2**self.dimension - self.dimension - 1,), device=device)
+        counter = 0
+        for i in range(self.dimension):
+            for j in range(2**i+1, 2**(i+1)):
+                self.action_map[counter] = j
+                counter += 1
+
+    def forward(self, x):
+        r = self.action_map[torch.randint(2**self.dimension - self.dimension - 1, (x.shape[0],), device=self.device)]
+        return torch.nn.functional.one_hot(r.long(), num_classes=2**self.dimension).type(torch.float32)
 
 
 class AllCoordHostModule(nn.Module, DummyModule):
