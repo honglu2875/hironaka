ab011be3529dc9039ef50097c3348288a5f52c20
Honglu Fan <honglu2875@gmail.com>
2022-10-16 17:12:00 -0400

    fix sign.

--- hironaka/jax/util.py
+++ hironaka/jax/util.py
@@ -277,7 +277,7 @@ def calculate_value_using_reward_fn(
     discounted_value = vmap(jnp.matmul, (None, 0), 0)(discount_table, reward)
 
     # Estimate the value for unfinished games.
-    sign = (-1) ** (num_points + 1) if use_unified_tree else 1
+    sign = (-1) ** (max_length_game + 1) if use_unified_tree else 1
     est_values = est_fn(value_prior[:, -1], num_points[:, -1])[:, None] * sign
     unfinished = (~done[:, -1:] * est_values) * (discount ** jnp.arange(max_length_game)[::-1])[None, :]
 
